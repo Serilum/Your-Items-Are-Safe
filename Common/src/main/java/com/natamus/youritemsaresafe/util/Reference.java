f9c22cfd41698a0c35a7ed40255408cdd5809a7e
Rick South <rick@natamus.com>
2023-12-18 09:53:36 +0100

    (v3.6) Improved mod icon compression, which lowers the jar file size. [+2]

--- Common/src/main/java/com/natamus/youritemsaresafe/util/Reference.java
+++ Common/src/main/java/com/natamus/youritemsaresafe/util/Reference.java
@@ -3,6 +3,6 @@ package com.natamus.youritemsaresafe.util;
 public class Reference {
 	public static final String MOD_ID = "youritemsaresafe";
 	public static final String NAME = "Your Items Are Safe";
-	public static final String VERSION = "3.5";
+	public static final String VERSION = "3.6";
 	public static final String ACCEPTED_VERSIONS = "[1.19.2]";
 }
