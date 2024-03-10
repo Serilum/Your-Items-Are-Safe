fe4ecd0f827816727ac806401bf7695a782d7445
Rick South <rick@natamus.com>
2024-03-10 16:33:59 +0100

    (v4.2) Updated mod to work with the new player head caching feature in Collective. [+1]

--- Common/src/main/java/com/natamus/youritemsaresafe/util/Reference.java
+++ Common/src/main/java/com/natamus/youritemsaresafe/util/Reference.java
@@ -3,6 +3,6 @@ package com.natamus.youritemsaresafe.util;
 public class Reference {
 	public static final String MOD_ID = "youritemsaresafe";
 	public static final String NAME = "Your Items Are Safe";
-	public static final String VERSION = "4.1";
+	public static final String VERSION = "4.2";
 	public static final String ACCEPTED_VERSIONS = "[1.20.1]";
 }
