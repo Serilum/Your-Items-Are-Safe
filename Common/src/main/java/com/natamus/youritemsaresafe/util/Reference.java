8bc86f8bf11a247e7d43e8212d7cdcd701c7f890
Rick South <rick@natamus.com>
2024-04-28 01:18:57 +0200

    Your Items Are Safe source code for 1.20.5.

---
+++ Common/src/main/java/com/natamus/youritemsaresafe/util/Reference.java
@@ -0,0 +1,8 @@
+package com.natamus.youritemsaresafe.util;
+
+public class Reference {
+	public static final String MOD_ID = "youritemsaresafe";
+	public static final String NAME = "Your Items Are Safe";
+	public static final String VERSION = "4.4";
+	public static final String ACCEPTED_VERSIONS = "[1.20.5]";
+}
---
+++ NeoForge/src/main/java/com/natamus/youritemsaresafe/ModNeoForge.java
@@ -0,0 +1,29 @@
+package com.natamus.youritemsaresafe;
+
+import com.natamus.collective.check.RegisterMod;
+import com.natamus.youritemsaresafe.neoforge.config.IntegrateNeoForgeConfig;
+import com.natamus.youritemsaresafe.neoforge.events.NeoForgeDeathEvent;
+import com.natamus.youritemsaresafe.util.Reference;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.ModLoadingContext;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.event.lifecycle.FMLLoadCompleteEvent;
+import net.neoforged.neoforge.common.NeoForge;
+
+@Mod(Reference.MOD_ID)
+public class ModNeoForge {
+	
+	public ModNeoForge(IEventBus modEventBus) {
+		modEventBus.addListener(this::loadComplete);
+
+		ModCommon.init();
+
+		IntegrateNeoForgeConfig.registerScreen(ModLoadingContext.get());
+
+		RegisterMod.register(Reference.NAME, Reference.MOD_ID, Reference.VERSION, Reference.ACCEPTED_VERSIONS);
+	}
+
+	private void loadComplete(final FMLLoadCompleteEvent event) {
+		NeoForge.EVENT_BUS.register(NeoForgeDeathEvent.class);
+	}
+}
