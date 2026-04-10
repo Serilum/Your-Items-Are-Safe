534587bedcf2825918774f31f1a70cb307da00c2
Rick South <rick@natamus.com>
2026-04-10 02:39:36 +0200

    Your Items Are Safe source code for 26.1.2.

---
+++ Common/src/main/java/com/natamus/youritemsaresafe/util/Reference.java
@@ -0,0 +1,8 @@
+package com.natamus.youritemsaresafe.util;
+
+public class Reference {
+	public static final String MOD_ID = "youritemsaresafe";
+	public static final String NAME = "Your Items Are Safe";
+	public static final String VERSION = "4.7";
+	public static final String ACCEPTED_VERSIONS = "[26.1.2]";
+}
